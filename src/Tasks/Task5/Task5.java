938143adb9c809bcd2db819dd7f92f771f53e152
YarikKomarov <music4yarik@gmail.com>
2024-06-11 17:25:34 +0300

    Task5

---
+++ src/Tasks/Task5/Task5.java
@@ -0,0 +1,22 @@
+package Tasks.Task5;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);    //scanner for input int value
+        System.out.print("Input \"x\" :");
+        int x = in.nextInt();
+        boolean result = evenInRange(x);        //created function for checking interval 24...32 without odd integers
+        System.out.println(result);
+    }
+
+    public static boolean evenInRange(int x){
+        if(x >= 24 & x <=32 ){                  //interval
+            if(x % 2 == 0){                     //excluding odd integers
+                return true;
+            }
+        }
+        return false;
+    }
+}
